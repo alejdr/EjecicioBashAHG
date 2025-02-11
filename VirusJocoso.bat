98ca35600a965d297c12dedfa083450128ed498f
alejdr <alejandroharogarcia03@gmail.com>
2025-02-11 14:38:08 +0100

    arreglo muestra imagen

--- VirusJocoso.bat
+++ VirusJocoso.bat
@@ -13,6 +13,21 @@ pause > nul
 
 timeout /t 8 /nobreak >nul
 
-for /l %%i in (1,1,5) do (
-    start "" "troliado.jpg"
-)
+@echo off
+title ¡Ordenador infectado!
+color 0a
+cls
+
+rem Obtener el nombre del usuario
+set USERNAME=%username%
+
+echo. ¡Manos en el aire todo el mundo que este ordenador ha sido infectado!
+echo. %USERNAME% hay que ser mongolo para abrir un archivo de este tipo.
+echo. Mira que pone virus en el nombre...
+pause > nul
+
+timeout /t 8 /nobreak >nul
+
+@echo off
+for /L %%i in (1,1,5) do start "" "%~dp0troliao.jpg"
+
