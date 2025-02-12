57f34a45e451f3736c77d3cd3ed088a34f234378
alejdr <alejandroharogarcia03@gmail.com>
2025-02-12 10:49:24 +0100

    eliminación de la muestra de imagenes, estaba mal hecha. Configuro el nombre de la imagen y su ruta

--- VirusJocoso.bat
+++ VirusJocoso.bat
@@ -13,21 +13,5 @@ pause > nul
 
 timeout /t 8 /nobreak >nul
 
-@echo off
-title ¡Ordenador infectado!
-color 0a
-cls
-
-rem Obtener el nombre del usuario
-set USERNAME=%username%
-
-echo. ¡Manos en el aire todo el mundo que este ordenador ha sido infectado!
-echo. %USERNAME% hay que ser mongolo para abrir un archivo de este tipo.
-echo. Mira que pone virus en el nombre...
-pause > nul
-
-timeout /t 8 /nobreak >nul
-
-@echo off
-for /L %%i in (1,1,5) do start "" "%~dp0troliao.jpg"
-
+rem Configurar el nombre y la ruta de la imagen
+set "imagen=%CD%\troliado.jpg"
